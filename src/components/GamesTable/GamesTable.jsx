c4f0fb4467763c6749494b1c91adce18def08578
juanvalag <juanvalag@unisabana.edu.co>
2022-09-28 17:29:56 -0500

    Adding basic games table styles

--- src/components/GamesTable/GamesTable.jsx
+++ src/components/GamesTable/GamesTable.jsx
@@ -1,8 +1,9 @@
 import { AgGridReact } from 'ag-grid-react';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.min.css';
+import './GamesTable.css';
 
 function GamesTable(props) {
     const pageSize = 10;
