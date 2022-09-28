359a0a74e5d3712a7acd8b7d6b5afd1671600d27
juanvalag <juanvalag@unisabana.edu.co>
2022-09-28 12:07:19 -0500

    Adding basic games list component configuration

--- src/components/GamesList/GamesList.jsx
+++ src/components/GamesList/GamesList.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import GamesTable from '../GamesTable/GamesTable'
 
 function GamesList(props) {
     return (
-        <div></div>
+        <div>
+            <h2>List of Games</h2>
+            <GamesTable />
+        </div>
     )
 }
 
