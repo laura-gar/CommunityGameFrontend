5f933421d4a4613ee165f75134ffd013285a88ae
juanvalag <juanvalag@unisabana.edu.co>
2022-09-27 19:28:32 -0500

    Added empty base components GamesList and GamesTable

---
+++ src/components/GamesList/GamesList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GamesList(props) {
+    return (
+        <div></div>
+    )
+}
+
+export default GamesList
---
+++ src/components/GamesTable/GamesTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GamesTable(props) {
+    return (
+        <div></div>
+    )
+}
+
+export default GamesTable
