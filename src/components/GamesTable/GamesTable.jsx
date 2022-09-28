e50f9e2cc2eb346a81023f9955ad7b8b49e5a567
juanvalag <juanvalag@unisabana.edu.co>
2022-09-28 17:24:47 -0500

    Adding basic games table sizing

--- src/components/GamesTable/GamesTable.jsx
+++ src/components/GamesTable/GamesTable.jsx
@@ -15,6 +15,7 @@ function GamesTable(props) {
     ]);
 
     const onGridReady = useCallback(({ api }) => {
+        api.sizeColumnsToFit();
         const dataSource = {
             getRows: ({ startRow, endRow, successCallback, failCallback }) => {
                 const numRequiredGames = endRow - startRow;
@@ -39,14 +40,15 @@ function GamesTable(props) {
     }, []);
 
     return (
-        <div className='ag-theme-alpine' style={{ height: 400, width: 600 }}>
+        <div className='ag-theme-alpine' style={{ height: '100%', width: '100%' }}>
             <AgGridReact
                 rowData={games}
                 columnDefs={columnDefs}
                 paginationPageSize={pageSize}
                 pagination={true}
                 rowModelType={'infinite'}
-                onGridReady={onGridReady} >
+                onGridReady={onGridReady}
+                domLayout='autoHeight' >
             </AgGridReact>
         </div>
     );
