4ab74bbe18f0c8b1ca4807832048d9c241a7675c
juanvalag <juanvalag@unisabana.edu.co>
2022-09-29 01:22:29 -0500

    Adding bootstrap styles to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {
   BrowserRouter, Route, Routes
 } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 
 
