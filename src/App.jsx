d2017df4387604ac2e27004360a732aad4282f65
juanvalag <juanvalag@unisabana.edu.co>
2022-09-28 22:28:50 -0500

    adding basic app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter, Route, Routes
+} from "react-router-dom";
 import './App.css';
 
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/login'} element={<div></div>} />
+          <Route path={'/signup'} element={<div></div>} />
+          <Route path={'/games'} element={<div></div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
