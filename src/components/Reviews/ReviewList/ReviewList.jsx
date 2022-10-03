6134d513a71359519e1df72655d5b5162977098c
Juan David Valencia Agudelo <53841307+juanvalag2019@users.noreply.github.com>
2022-10-03 00:50:48 -0500

    Removing add review button if the user has reviewed a game (#28)

--- src/components/Reviews/ReviewList/ReviewList.jsx
+++ src/components/Reviews/ReviewList/ReviewList.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../../hooks/useAuth';
 import reviewService from '../../../services/Review/reviewService';
 import Review from '../Review/Review';
 import ReviewModal from '../ReviewModal/ReviewModal';
 
 function ReviewList({ gameId }) {
+    const auth = useAuth();
     const [reviews, setReviews] = useState([]);
-    const [areReviewsChanges, setChangesReviews] = useState(false); 
+    const [areReviewsChanges, setChangesReviews] = useState(false);
     useEffect(() => {
         reviewService.getReviewsByGame(gameId,
             (reviews) => {
@@ -16,13 +18,19 @@ function ReviewList({ gameId }) {
             }
         );
     }, [gameId, areReviewsChanges]);
-    const reviewsToRender = reviews.map((reviewData) => <Review key={reviewData.id} review={reviewData} change={setChangesReviews}/>);
+    let userHasReviewed = false;
+    const reviewsToRender = reviews.map((reviewData) => {
+        if (reviewData.user?.id === auth.user.id) {
+            userHasReviewed = true;
+        }
+        return <Review key={reviewData.id} review={reviewData} change={setChangesReviews} />
+    });
     return (
         <div className='card p-5'>
             <div className="row mb-4">
                 <div className="col d-flex justify-content-between">
                     <h2>Reviews</h2>
-                    <ReviewModal gameId={gameId} change={setChangesReviews}/>
+                    {!userHasReviewed ? <ReviewModal gameId={gameId} change={setChangesReviews} /> : null}
                 </div>
             </div>
             <div className="row">
